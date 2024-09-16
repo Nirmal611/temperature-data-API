c1c4b8e9e1ebb778cf502d7c7e66bf14860f3d9c
gitmal <pnirmal896@gmail.com>
2024-09-17 01:55:29 +0530

    Add additional URL endpoints

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ app = Flask(__name__)
 
 stations = pd.read_csv('data_small/stations.txt', skiprows=17)
 stations = stations[['STAID', 'STANAME                                 ']]
+
+
 @app.route('/')
 def home():
     return render_template('home.html', data = stations.to_html())
 
 
-@app.route('/<station_id>/<date>/')
+@app.route('/<station_id>/date=<date>/')
 def weather_data(station_id,date):
     filename = "data_small/TG_STAID"+str(station_id).zfill(6)+".txt"
     df = pd.read_csv(filename , skiprows=20 , parse_dates=['    DATE'])
@@ -32,6 +34,29 @@ def weather_data(station_id,date):
 
     return {'station_id':station_id, 'station': station, 'date': date, 'temperature': temperature ,
             'year_mean': year_mean, 'month_mean': month_mean}
+
+@app.route('/<station_id>/year=<year>/')
+def yearly_data(station_id , year):
+    filename = "data_small/TG_STAID" + str(station_id).zfill(6) + ".txt"
+    df = pd.read_csv(filename , skiprows=20 )
+    df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
+    df['DATE'] = df['    DATE'].astype(str)
+    x = df.loc[df['DATE'].str.startswith(str(year))]
+    x = x[['DATE','STAID', 'TG',]]
+
+    return x.to_dict(orient = 'records')
+
+
+@app.route('/<station_id>/')
+def station_data(station_id):
+    filename = "data_small/TG_STAID" + str(station_id).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df['DATE'] = df['    DATE']
+    df['TG'] = df['   TG'].mask(df['   TG'] == -9999, np.nan) / 10
+    x = df.drop([' Q_TG',' SOUID','   TG','    DATE'],axis=1)
+
+    return x.to_dict(orient='records')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
